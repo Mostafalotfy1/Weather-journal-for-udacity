03ae5cd42b8ccba83a1186d6de1db1ee77ce39a6
Mostafa Lotfy <36739139+Mostafalotfy1@users.noreply.github.com>
2021-10-10 02:43:18 +0200

    Weather app

---
+++ website/app.js
@@ -0,0 +1,61 @@
+/* Global Variables */
+let url = 'http://api.openweathermap.org/data/2.5/forecast?zip=';
+let apiKey = '&appid=ebfb7b7e7e8e84d1711e53acfe74a580';
+let generator = document.getElementById('generate');
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+//event
+generator.addEventListener('click', Action);
+function Action (e){
+    const newZip = document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
+    console.log(newZip)
+    getWeatherData(url,newZip,apiKey).then(function(data){
+       console.log(data);
+       getData('/add',{date:newDate , temp:data.list[0].main.temp, content:feelings}) 
+       finallResualt();
+    })
+};
+const getWeatherData = async(url , zip , key)=>{
+    console.log(url+zip+key)
+    const response = await fetch(url+zip+key)
+    try{
+        const data = await response.json();
+        return data;
+    }catch(e){
+        console.log(e);
+    }
+}
+const getData = async (url = '' , data = {})=>{
+    console.log(data);
+    const res = await fetch(url , {
+        method:'POST',
+        credentials:"same-origin",
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(data)
+    });
+    try{
+        const newData = await res.json();
+        console.log(newData);
+        return newData;
+    }catch(e){
+        console.log(e);
+    }
+}
+//show data
+const finallResualt = async ()=>{
+    const req = await fetch('/all');
+    console.log(req)
+    try{
+        const allData = await req.json();
+        console.log(allData.Elements.date)
+        document.getElementById('date').innerHTML = `Date : ${allData.Elements.date}`;
+        document.getElementById('temp').innerHTML = `Temperature : ${allData.Elements.temp}`;
+        document.getElementById('content').innerHTML = `I feel : ${allData.Elements.content}`;
+    }catch(e){
+        console.log(e)
+    }
+}
